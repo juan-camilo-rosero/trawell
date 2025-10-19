5031a1a63851526a43acba1c5827e904fa74f54d
juan-camilo-rosero <juroseros@unal.edu.co>
2025-10-19 10:05:50 -0500

    Add firebase configuration for auth

---
+++ src/lib/firebase.config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseApiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+const firebaseAuthDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
+const firebaseProjectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+const firebaseStorageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+const firebaseSenderId = process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID;
+const firebaseAppId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
+
+const firebaseConfig = {
+  apiKey: firebaseApiKey,
+  authDomain: firebaseAuthDomain,
+  projectId: firebaseProjectId,
+  storageBucket: firebaseStorageBucket,
+  messagingSenderId: firebaseSenderId,
+  appId: firebaseAppId
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
