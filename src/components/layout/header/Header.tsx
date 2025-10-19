d42237db318fca09f20ef8c9d84ef7ad207677e2
juan-camilo-rosero <juroseros@unal.edu.co>
2025-10-18 21:22:46 -0500

    fix minor bug

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import { RiMenu3Fill } from "react-icons/ri";
-import { useContext } from "react";
 
 function Header() {
   return (
-    <header className="bg-secondary-200 p-4 w-screen fixed flex flex-row items-center justify-between lg:py-3 z-30">
+    <header className="bg-secondary-200 py-4 custom-ph w-screen fixed flex flex-row items-center justify-between lg:py-3 z-30">
       <div className="md:max-w-xl w-full flex flex-row items-center justify-between">
         <div>
             <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png" alt="Logo pinterest" className="w-8 h-8"/>
