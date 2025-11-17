4e6c7ffab027b4d132471d548a324e8e827b1ee8
juan-camilo-rosero <juroseros@unal.edu.co>
2025-11-17 13:38:47 -0500

    Fix minor bug

--- src/models/itinerary/Itinerary.ts
+++ src/models/itinerary/Itinerary.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, Document, Model, Types } from 'mongoose';
+import mongoose, { Schema, Document, Model } from 'mongoose';
 import { IDay, ISearchParams } from './interfaces';
 
 export interface IItineraryDocument extends Document {
